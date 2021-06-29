349d41956d2e3793c4051b217058afcc3d213d29
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2021-06-29 17:04:04 +0200

    Working controller deployment

--- linstor-controller/src/charm.py
+++ linstor-controller/src/charm.py
@@ -11,94 +11,174 @@ develop a new k8s charm using the Operator Framework:
 
     https://discourse.charmhub.io/t/4208
 """
-
+import json
 import logging
 
-from ops.charm import CharmBase
-from ops.framework import StoredState
-from ops.main import main
-from ops.model import ActiveStatus
+import toml
+from oci_image import OCIImageResource, OCIImageResourceError
+from ops import charm, framework, main, model
 
 logger = logging.getLogger(__name__)
 
 
-class LinstorControllerCharm(CharmBase):
-    """Charm the service."""
-
-    _stored = StoredState()
+class LinstorSatelliteCharm(charm.CharmBase):
+    _stored = framework.StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+        self._stored.set_default(db_host=None, db_port=None, db_database=None, db_user=None, db_password=None)
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+        self.framework.observe(self.on.database_relation_changed, self._on_database_relation_changed)
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
-                }
+        self.linstor_controller_image = OCIImageResource(self, "linstor-controller-image")
+        self.framework.observe(self.on.install, self._set_pod_spec)
+        self.framework.observe(self.on.upgrade_charm, self._set_pod_spec)
+        self.framework.observe(self.on.config_changed, self._set_pod_spec)
+
+    def _set_pod_spec(self, event: charm.HookEvent):
+        if not self.unit.is_leader():
+            return
+
+        try:
+            linstor_controller_image = self.linstor_controller_image.fetch()
+        except OCIImageResourceError as e:
+            self.unit.status = e.status
+            event.defer()
+            return
+
+        if not self._stored.db_database:
+            self.unit.status = model.BlockedStatus("waiting for database relation")
+            event.defer()
+            return
+
+        # TODO: linstor client conf + HTTP endpoint
+        linstor_conf = toml.dumps({
+            "db": {
+                "connection_url":
+                    f"jdbc:postgresql://{self._stored.db_host}:{self._stored.db_port}/{self._stored.db_database}",
+                "user": self._stored.db_user,
+                "password": self._stored.db_password,
             },
+        })
+
+        linstor_election_env = {
+            "K8S_AWAIT_ELECTION_ENABLED": 1,
+            "K8S_AWAIT_ELECTION_NAME": self.app.name,
+            "K8S_AWAIT_ELECTION_LOCK_NAME": self.app.name,
+            "K8S_AWAIT_ELECTION_LOCK_NAMESPACE": {"field": {"path": "metadata.namespace", "api-version": "v1"}},
+            "K8S_AWAIT_ELECTION_IDENTITY": {"field": {"path": "metadata.name", "api-version": "v1"}},
+            "K8S_AWAIT_ELECTION_POD_IP": {"field": {"path": "status.podIP", "api-version": "v1"}},
+            "K8S_AWAIT_ELECTION_NODE_NAME": {"field": {"path": "spec.nodeName", "api-version": "v1"}},
+            "K8S_AWAIT_ELECTION_SERVICE_NAME": self.app.name,
+            "K8S_AWAIT_ELECTION_SERVICE_NAMESPACE": {"field": {"path": "metadata.namespace", "api-version": "v1"}},
+            "K8S_AWAIT_ELECTION_SERVICE_PORTS_JSON": json.dumps(
+                [{"name": "linstor-controller", "port": self.config["linstor-http-port"]}]
+            ),
+            "K8S_AWAIT_ELECTION_STATUS_ENDPOINT": ":9999",
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
 
-    def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+        self.unit.status = model.MaintenanceStatus("Setting pod spec")
+        self.model.pod.set_spec(
+            spec={
+                "version": 3,
+                "containers": [
+                    {
+                        "name": "linstor-controller",
+                        "imageDetails": linstor_controller_image,
+                        "args": ["startController"],
+                        "ports": [
+                            {"name": "linstor-api", "containerPort": self.model.config["linstor-http-port"]},
+                        ],
+                        "volumeConfig": [
+                            {
+                                "name": "linstor",
+                                "mountPath": "/etc/linstor",
+                                "files": [
+                                    {
+                                        "path": "linstor.toml",
+                                        "content": linstor_conf,
+                                    },
+                                ],
+                            }
+                        ],
+                        "envConfig": linstor_election_env,
+                    },
+                ],
+                "serviceAccount": {
+                    "roles": [
+                        {
+                            "name": "linstor-controller",
+                            "rules": [
+                                {
+                                    "apiGroups": ["coordination.k8s.io"],
+                                    "resources": ["leases"],
+                                    "verbs": [
+                                        "get",
+                                        "list",
+                                        "watch",
+                                        "delete",
+                                        "update",
+                                        "create",
+                                    ],
+                                },
+                                {
+                                    "apiGroups": [""],
+                                    "resources": ["endpoints", "endpoints/restricted"],
+                                    "verbs": [
+                                        "create",
+                                        "patch",
+                                        "update",
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+            k8s_resources={
+                "kubernetesResources": {
+                    "services": [
+                        {
+                            "name": self.app.name,
+                            "spec": {
+                                "type": "ClusterIP",
+                                "clusterIP": "",
+                                "ports": [
+                                    {
+                                        "name": "linstor-api",
+                                        "protocol": "TCP",
+                                        "port": self.model.config["linstor-http-port"],
+                                        "targetPort": self.model.config["linstor-http-port"],
+                                    },
+                                ],
+                            },
+                        },
+                    ],
+                },
+            },
+        )
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+        self.unit.status = model.ActiveStatus()
 
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+    def _db_name(self):
+        return self.config["dbname"]
 
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
+    def _on_database_relation_changed(self, event: charm.RelationChangedEvent):
+        if not self.unit.is_leader():
+            return
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
+        if event.relation.data[self.app].get("database") != self.app.name:
+            event.relation.data[self.app].update({"database": self.app.name})
 
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
-        else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+        self._stored.db_host = event.relation.data[event.app].get("host")
+        self._stored.db_port = event.relation.data[event.app].get("port")
+        self._stored.db_database = event.relation.data[event.app].get("database")
+        self._stored.db_user = event.relation.data[event.app].get("user")
+        self._stored.db_password = event.relation.data[event.app].get("password")
+
+        self._set_pod_spec(event)
 
 
 if __name__ == "__main__":
-    main(LinstorControllerCharm)
+    main.main(LinstorSatelliteCharm)
--- linstor-satellite/src/charm.py
+++ linstor-satellite/src/charm.py
@@ -27,9 +27,7 @@ class LinstorSatelliteCharm(charm.CharmBase):
         super().__init__(*args)
 
         self.linstor_satellite_image = OCIImageResource(self, "linstor-satellite-image")
-        self.drbd_monitoring_image = OCIImageResource(self, "drbd-monitoring-image")
         self.drbd_injector_image = OCIImageResource(self, "drbd-injector-image")
-        self.kube_rbac_proxy_image = OCIImageResource(self, "kube-rbac-proxy-image")
 
         self.framework.observe(self.on.install, self._set_pod_spec)
         self.framework.observe(self.on.upgrade_charm, self._set_pod_spec)
@@ -42,12 +40,35 @@ class LinstorSatelliteCharm(charm.CharmBase):
         try:
             linstor_satellite_image = self.linstor_satellite_image.fetch()
             drbd_injector_image = self.drbd_injector_image.fetch()
-            drbd_monitoring_image = self.drbd_monitoring_image.fetch()
-            kube_rbac_proxy_image = self.kube_rbac_proxy_image.fetch()
         except OCIImageResourceError as e:
             self.model.unit.status = e.status
             event.defer()
             return
+
+        injector_volumes = [
+            {
+                "name": "device-dir",
+                "mountPath": "/dev",
+                "hostPath": {"path": "/dev", "type": "Directory"},
+            },
+            {
+                "name": "modules-dir",
+                "mountPath": "/lib/modules",
+                "hostPath": {"path": "/lib/modules", "type": "Directory"},
+            },
+        ]
+        injector_env = {
+            "LB_FAIL_IF_USERMODE_HELPER_NOT_DISABLED": "yes",
+            "LB_HOW": "shipped_modules"
+        }
+
+        if self.config["compile-module"]:
+            injector_env["LB_HOW"] = "compile"
+            injector_volumes.append({
+                "name": "kernel-src-dir",
+                "mountPath": "/usr/src",
+                "hostPath": {"path": "/usr/src", "type": "Directory"},
+            })
 
         self.model.unit.status = model.MaintenanceStatus("Setting pod spec")
         self.model.pod.set_spec(
@@ -59,7 +80,7 @@ class LinstorSatelliteCharm(charm.CharmBase):
                         "imageDetails": linstor_satellite_image,
                         "args": ["startSatellite"],
                         "ports": [
-                            {"name": "linstor-control", "containerPort": self.model.config["linstor-control-port"]},
+                            {"name": "linstor-control", "containerPort": self.config["linstor-control-port"]},
                         ],
                         "kubernetes": {"securityContext": {"privileged": True}},
                         "volumeConfig": [
@@ -85,45 +106,8 @@ class LinstorSatelliteCharm(charm.CharmBase):
                         "init": True,
                         "imageDetails": drbd_injector_image,
                         "kubernetes": {"securityContext": {"privileged": True}},
-                        "volumeConfig": [
-                            {
-                                "name": "device-dir",
-                                "mountPath": "/dev",
-                                "hostPath": {"path": "/dev", "type": "Directory"},
-                            },
-                            {
-                                "name": "modules-dir",
-                                "mountPath": "/lib/modules",
-                                "hostPath": {"path": "/lib/modules", "type": "Directory"},
-                            },
-                            {
-                                "name": "kernel-src-dir",
-                                "mountPath": "/usr/src",
-                                "hostPath": {"path": "/usr/src", "type": "Directory"},
-                            },
-                        ],
-                        "envConfig": {
-                            "LB_FAIL_IF_USERMODE_HELPER_NOT_DISABLED": "yes",
-                            "LB_HOW": "compile"
-                        }
-                    },
-                    {
-                        "name": "drbd-monitoring",
-                        "imageDetails": drbd_monitoring_image,
-                        "args": [],
-                        # TODO: drbd-reactor-config
-                    },
-                    {
-                        "name": "kube-rbac-proxy",
-                        "imageDetails": kube_rbac_proxy_image,
-                        "args": [
-                            f"--secure-listen-address=0.0.0.0:{self.model.config['metrics-port']}",
-                            "--upstream=http://127.0.0.1:19942",
-                            "--logtostderr=true",
-                        ],
-                        "ports": [
-                            {"name": "metrics", "containerPort": self.model.config["metrics-port"]},
-                        ],
+                        "volumeConfig": injector_volumes,
+                        "envConfig": injector_env,
                     },
                 ],
                 "serviceAccount": {
